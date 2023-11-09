bb3278741b80ac58633d4a9a596133a8b1fbfb94
Hyacinthe Hamon <hyacinthehamon@Hyacinthes-MacBook-Air.local>
2023-11-09 12:35:14 +0800

    Add hmac & regex validation to Shopify OAuth integration

--- apps/dashboard/pages/api/integrations/shopify/oauth-callback.ts
+++ apps/dashboard/pages/api/integrations/shopify/oauth-callback.ts
@@ -4,6 +4,8 @@ import { AppNextApiRequest } from '@chaindesk/lib/types/index';
 import { ServiceProviderType } from '@chaindesk/prisma';
 import { prisma } from '@chaindesk/prisma/client';
 import axios from 'axios';
+import url from 'url';
+import hmacValidator from 'hmac-validator';
 
 const handler = createAuthApiHandler();
 
@@ -20,44 +22,62 @@ export const auth = async (req: AppNextApiRequest, res: NextApiResponse) => {
 
   const regex = /^[a-z\d_.-]+[.]myshopify[.]com$/;
 
+  var validate = hmacValidator({
+    replacements: {
+      both: {
+        '&': '%26',
+        '%': '%25',
+      },
+      keys: {
+        '=': '%3D',
+      },
+    },
+    excludedKeys: ['signature', 'hmac'],
+    algorithm: 'sha256',
+    format: 'hex',
+    digestKey: 'hmac',
+  });
+
   if (code && hmac && host && shop) {
-    // validate hmac & regex
-    // if (regex) {
-    // Exchange temporary code for a permanent access token
-    let accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+    let urlObj = req.url && url.parse(req.url);
+    let query = urlObj && urlObj?.search?.slice(1);
 
-    await axios
-      .post(accessTokenRequestUrl, {
-        client_id: appId,
-        client_secret: appSecret,
-        code,
-      })
-      .then(async (response) => {
-        let accessToken = response.data.access_token;
-        console.log('shop token ' + accessToken);
+    // Validate hmac signature & regex
+    if (shop.match(regex) && validate(appSecret, null, query)) {
+      // Exchange temporary code for a permanent access token
+      let accessTokenRequestUrl =
+        'https://' + shop + '/admin/oauth/access_token';
 
-        await prisma.serviceProvider.create({
-          data: {
-            type: ServiceProviderType.shopify,
-            name: null,
-            accessToken: accessToken,
-            refreshToken: null,
-            organization: {
-              connect: {
-                id: session?.organization?.id,
+      await axios
+        .post(accessTokenRequestUrl, {
+          client_id: appId,
+          client_secret: appSecret,
+          code,
+        })
+        .then(async (response) => {
+          let accessToken = response.data.access_token;
+
+          await prisma.serviceProvider.create({
+            data: {
+              type: ServiceProviderType.shopify,
+              name: null,
+              accessToken: accessToken,
+              refreshToken: null,
+              organization: {
+                connect: {
+                  id: session?.organization?.id,
+                },
               },
             },
-          },
+          });
+
+          res.redirect('/close-window');
+        })
+        .catch((error) => {
+          console.log(error);
+          res.redirect('/close-window');
         });
-
-        res.redirect('/close-window');
-      })
-      .catch((error) => {
-        console.log(error);
-        res.redirect('/close-window');
-      });
-
-    // }
+    }
   }
 };
 
